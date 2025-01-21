83f1ab0026a5c8c102ad26c0f52d0875c93a510e
Stoictaurus <stoictaurus@gmail.com>
2025-01-21 22:27:19 +0100

    Movement: adjusted camera move to player move

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,6 +4,8 @@ namespace Player
 {
     public class PlayerMovement: MonoBehaviour
     {
+        private const float CAMERA_OFFSET = -0.5f;
+        
         [SerializeField] private float movementSpeed = 5f;
         private Vector2 _playerMoveVector = Vector2.zero;
         private Camera _camera;
@@ -29,6 +31,9 @@ namespace Player
             var speed = movementSpeed * Time.deltaTime;
             var speedVector = new Vector3(_playerMoveVector.x * speed, 0f, _playerMoveVector.y * speed);
             gameObject.transform.position = gameObject.transform.position + speedVector;
+            
+            var cameraPosition = new Vector3(gameObject.transform.position.x, _camera.transform.position.y, gameObject.transform.position.z + CAMERA_OFFSET);
+            _camera.transform.position = cameraPosition;
         }
         
         private void OnMoveInput(Vector2 moveVector)
